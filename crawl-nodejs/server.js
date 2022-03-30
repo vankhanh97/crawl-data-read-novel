ca0b58e6c539256229917c0ea9394b5a31027848
Nguyen Van Khanh <khanhnv@rikkeisoft.com>
2022-03-30 13:38:20 +0700

    change-deploy

--- crawl-nodejs/server.js
+++ crawl-nodejs/server.js
@@ -23,6 +23,8 @@ app.get('/',function(req,res){
     })
 });
 app.use('/', router);
-app.listen(process.env.port || 3000);
+const port = process.env.PORT || '3000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
+
 
 
