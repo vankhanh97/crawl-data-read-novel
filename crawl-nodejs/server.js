bd6c4ccbca74b63acbecc86d33be91399652ae02
Nguyen Van Khanh <khanhnv@rikkeisoft.com>
2022-03-30 14:40:00 +0700

    'add'

--- crawl-nodejs/server.js
+++ crawl-nodejs/server.js
@@ -4,6 +4,13 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 const request = require('request-promise');
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -22,6 +29,18 @@ app.get('/',function(req,res){
         });
     })
 });
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/db', results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
 app.use('/', router);
 const port = process.env.PORT || '3000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
