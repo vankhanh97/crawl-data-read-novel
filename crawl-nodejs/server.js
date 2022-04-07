21fe461ca0d25d0b9510553d7bd723732172fd8d
Nguyen Van Khanh <khanhnv@rikkeisoft.com>
2022-04-07 21:45:20 +0700

    'changecss'

--- crawl-nodejs/server.js
+++ crawl-nodejs/server.js
@@ -34,12 +34,10 @@ app.get('/',async function(req,res){
     request('https://metruyenchu.com/truyen/ta-chi-muon-an-tinh-lam-cau-dao-ben-trong-nguoi/chuong-'+chapter, (error, response, html) => {
     }).then((data) => {
         const $ = cheerio.load(data); // load HTML
-        const regExp = /[a-zA-Z0-9][.][a-zA-Z0-9]/g;
+        const regExp = /(?<=[a-z0-9A-z])([.])(?=[a-z0-9A-z])/g;
         let text1 = $('.nh-read__title').text();
         // let text2 = $('.nh-read__title').text();
-        let text3 = $('.nh-read__content').text().replaceAll('."','."\n');
-        text3.replaceAll('. ','.\n');
-        text3.replaceAll(regExp,'.\n');
+        let text3 = $('.nh-read__content').text().replaceAll('."','."\n').replaceAll('. ','.\n');
         console.log(text3.replaceAll(regExp,'.\n'))
         res.render('index', {
             title1: text1,
