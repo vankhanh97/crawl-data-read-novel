bdf97640f1b60fe1811902469168c616e6be5b24
Nguyen Van Khanh <khanhnv@rikkeisoft.com>
2022-04-13 14:07:16 +0700

    'changejs'

--- crawl-nodejs/server.js
+++ crawl-nodejs/server.js
@@ -37,7 +37,7 @@ app.get('/',async function(req,res){
         // const regExp = /(?<=[a-z0-9A-z])([.])(?=[a-z0-9A-z])/g;
         let text1 = $('.nh-read__title').text();
         // let text2 = $('.nh-read__title').text();
-        let text3 = $('.nh-read__content').text().replaceAll('."','."\n').replaceAll('. ','.\n');
+        let text3 = $('.nh-read__content').text();
         // console.log(text3.replaceAll(regExp,'.\n'))
         res.render('index', {
             title1: text1,
